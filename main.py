97ec1ff0c19d8bf4f4a0ab57abf222e21bbd621b
Aleksandra <aleksandra_sidorova06@mail.ru>
2025-12-18 13:38:03 +0400

    commit

--- main.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
-engine = create_async_engine('sqlite+aiosqlite:///books.db')
-
-new_session = async_sessionmaker(engine)
